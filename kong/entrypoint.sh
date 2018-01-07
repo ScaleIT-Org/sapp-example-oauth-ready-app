0f9c0c093dbc796e99bbcda395f058256435ed5e
Patrick Piatkowski <patrick.piatkowski@gmail.com>
2018-01-07 20:11:25 +0100

    fix kong entrypoint not applying config

--- kong/entrypoint.sh
+++ kong/entrypoint.sh
@@ -21,28 +21,8 @@ else
     echo "Database preparation finished"
 fi
 
-# TODO File not recognized
-if [ -f /config/kong-apis.json ]; then
-    echo "APIs config file detected, applying"
-
-    for row in $(jq -c '.[]' /config/kong-apis.json); do
-        # skip empty rows
-        if [[ -z "${row// }" ]]; then
-            continue
-        fi
-
-        name=$(echo $row | jq -r '.name')
-        # check if API with name already exists otherwise update it
-        res=$(curl -s -o /dev/null -w '%{http_code}\n' localhost:8001/apis/${name})
-        if [ $res == 404 ]; then
-            echo "Adding API ${name}:"
-            curl -s -d ${row} -H "Content-Type: application/json" -X POST http://localhost:8001/apis | jq .
-        else
-            echo "Updating API ${name}:"
-            curl -s -d ${row} -H "Content-Type: application/json" -X PATCH http://localhost:8001/apis/${name} | jq .
-        fi
-
-    done
+if [ "$1" = "/usr/local/openresty/nginx/sbin/nginx" ]; then
+    sh apply-config.sh &
 fi
-
+    
 exec "$@"
